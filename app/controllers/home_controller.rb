5c32b70a75be2fcae0a4f8468527df79570046c4
Abhilash V <abhivannarath006@gmail.com>
2014-06-29 18:45:54 +0530

    changes in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,17 +19,19 @@ class HomeController < ApplicationController
 
   def create_user_prediction
     index		
-    match = Match.find params[:match_id]
+    @match = Match.find params[:match_id]
     options = params
     ['commit', 'action', 'utf8', 'controller'].each { |col|
       options.delete(col)
     }
-    @prediction = current_user.create_prediction(match, params[:team_a_score], params[:team_b_score], options)
+    @prediction = current_user.create_prediction(@match, params[:team_a_score], params[:team_b_score], options)
   	# DailyChallengesUser.create_prediction(params[:match_id],current_user.id, params[:daily_challenge_id], params[:team_a_score], params[:team_b_score], params[:result])
-		@id = "f#{params[:match_id]}"
+		@id = "match#{@match.game_id}"
+		
+		# partial_path = render "/home/football/match_details", :m => @match, :current_user => current_user, :dc_today => !match.daily_challenge
 		
 		respond_to do |format|
-		  flash[:notice] = "Successfully created prediction for match #{match.match}" 
+		  flash[:notice] = "Successfully created prediction for match #{@match.match}" 
 			format.js
 		end
 	end
